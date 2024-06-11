336ba988c4382767fb83bf07b693972dd8123d0b
Superneenee <olwynday@hotmail.com>
2024-06-11 18:02:34 +1000

    Celebrity Cats and Sounds

--- catscript.js
+++ catscript.js
@@ -3,7 +3,6 @@ function catImageClick(id) {
 	var input = document.getElementById(id);
 	var img = document.getElementById(id.replace('yes', ''));
 
-
 	if (input.checked) {
 		img.src = "CatFit" + id.replace("cats", "").replace("yes", "") + ".jpg";
 		img.style.display = "inline";
@@ -12,6 +11,45 @@ function catImageClick(id) {
 		img.src = "";
 		img.style.display = "none";
 	}
+}
+
+function catSoundOver(bigcat) {
+
+	var audio = document.getElementById('bigCatSound')
+	audio.muted = false;
+	audio.src = bigcat + "Sound.mp3"
+	audio.play();
+
+}
+
+const celebrityCats = ["Trumpet", "Boofa"]
+
+function showCelebrityCats() {
+
+	document.getElementById("celebrityCatList").innerHTML = "";
+
+	var ul = document.getElementById("celebrityCatList");
+
+	for (let i = 0; i < celebrityCats.length; i++) {
+
+		var li = document.createElement("li");
+		li.appendChild(document.createTextNode(celebrityCats[i]));
+		li.setAttribute("id", "element" + celebrityCats[i]);
+		ul.appendChild(li);
+	}
+
+}
+
+function catAdd() {
+
+	celebrityCats.push(catInput.value);
+	showCelebrityCats();
+}
+
+function catDel() {
+
+	celebrityCats.pop(catInput.value);
+	showCelebrityCats();
 }
 
 let slideIndex = 1;
