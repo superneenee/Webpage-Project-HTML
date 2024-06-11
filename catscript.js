10a2c55916ccb4c3d67dbe17e82f2d147100d610
Superneenee <olwynday@hotmail.com>
2024-06-11 16:16:15 +1000

    ImageClick

--- catscript.js
+++ catscript.js
@@ -1,45 +1,16 @@
-function cat1(){
+function catImageClick(id) {
 
-var img = document.getElementById('cats1');
-img.src = "";
-	if (cats1yes.checked) {
-	img.src = "CatFit1.jpg";
+	var input = document.getElementById(id);
+	var img = document.getElementById(id.replace('yes', ''));
+
+
+	if (input.checked) {
+		img.src = "CatFit" + id.replace("cats", "").replace("yes", "") + ".jpg";
+		img.style.display = "inline";
 	}
-}
-
-function cat2(){
-
-var img = document.getElementById('cats2');
-img.src = "";
-	if (cats2yes.checked) {
-	img.src = "CatFit2.jpg";
-	}
-}
-
-function cat3(){
-
-var img = document.getElementById('cats3');
-img.src = "";
-	if (cats3yes.checked) {
-	img.src = "CatFit3.jpg";
-	}
-}
-
-function cat4(){
-
-var img = document.getElementById('cats4');
-img.src = "";
-	if (cats4yes.checked) {
-	img.src = "CatFit4.jpg";
-	}
-}
-
-function cat5(){
-
-var img = document.getElementById('cats5');
-img.src = "";
-	if (cats5yes.checked) {
-	img.src = "CatFit5.jpg";
+	else {
+		img.src = "";
+		img.style.display = "none";
 	}
 }
 
@@ -47,17 +18,17 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+	showSlides(slideIndex += n);
 }
 
 function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  slides[slideIndex-1].style.display = "block";
+	let i;
+	let slides = document.getElementsByClassName("mySlides");
+	if (n > slides.length) { slideIndex = 1 }
+	if (n < 1) { slideIndex = slides.length }
+	for (i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex - 1].style.display = "block";
 }
 
